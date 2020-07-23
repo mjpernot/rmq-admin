5e821fe15354849be033337bab801d07c6ea81d9
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-23 08:30:32 +0100

    Add test/unit/rmq_admin/code_coverage.sh

---
+++ test/unit/rmq_admin/code_coverage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Unit test code coverage for module.
+# This will run the Python code coverage module against all unit test modules.
+# This will show the amount of code that was tested and which lines of code
+#	that was skipped during the test.
+
+coverage erase
+
+echo ""
+echo "Running unit test modules in conjunction with coverage"
+coverage run -a --source=rmq_admin test/unit/rmq_admin/help_message.py 
+
+echo ""
+echo "Producing code coverage report"
+coverage combine
+coverage report -m
+ 
