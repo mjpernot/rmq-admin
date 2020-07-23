ef122a2a0999a83234673670718eb36bc6a3979e
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-23 08:30:27 +0100

    Add test/unit/rmq_admin/unit_test_run.sh

---
+++ test/unit/rmq_admin/unit_test_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Unit testing program for the module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Unit test:  rmq_admin.py"
+test/unit/rmq_admin/help_message.py
+
