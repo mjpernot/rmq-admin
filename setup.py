1b48ec8c83b5b9c31e636529e50e94d823ce3c54
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-23 08:20:04 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,52 @@
+# Classification (U)
+
+"""Program:  setup.py
+
+    Description:  A setuptools based setup module.
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import os
+import setuptools
+
+# Third-party
+
+# Local
+import version
+
+
+# Read in long description from README file.
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md")) as f_hdlr:
+    LONG_DESCRIPTION = f_hdlr.read()
+
+setuptools.setup(
+    name="RabbitMQ_Admin",
+    description="RabbitMQ program to administrate RabbitMQ system.",
+    author="Mark Pernot",
+    author_email="Mark.J.Pernot@coe.ic.gov",
+    url="https://sc.appdev.proj.coe.ic.gov/JAC-DSXD/rmq-admin",
+    version=version.__version__,
+    platforms=["Linux"],
+    long_description=LONG_DESCRIPTION,
+
+    classifiers=[
+        # Common Values:
+        #  1 - Pre-Alpha
+        #  2 - Alpha
+        #  3 - Beta
+        #  4 - Field
+        #  5 - Production/Stable
+        "Development Status :: 1 - Pre-Alpha",
+        "Operating System :: Linux",
+        "Operating System :: Linux :: Centos",
+        "Operating System :: Linux :: Ubuntu",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Database",
+        "Topic :: Database :: RabbitMQ",
+        "Topic :: Database :: RabbitMQ :: 3.6.6"])
