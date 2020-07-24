7c26bb7d36db5b55e7b43c42cc75172266756338
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-24 10:38:54 +0100

    Update test/unit/rmq_admin/unit_test_run.sh v0.0.1

--- test/unit/rmq_admin/unit_test_run.sh
+++ test/unit/rmq_admin/unit_test_run.sh
@@ -9,5 +9,6 @@ echo "Unit test:  rmq_admin.py"
 test/unit/rmq_admin/create_base.py
 test/unit/rmq_admin/fill_body.py
 test/unit/rmq_admin/help_message.py
+test/unit/rmq_admin/node_health.py
 test/unit/rmq_admin/print_list.py
 
