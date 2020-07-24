c58679b9fe92c5e90415cc8e17136975f529ab51
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-24 11:50:24 +0100

    Added test/integration/rmq_admin/integration_test_run.sh v0.0.1

---
+++ test/integration/rmq_admin/integration_test_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Unit testing program for the module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Integration test:  rmq_admin.py"
+
