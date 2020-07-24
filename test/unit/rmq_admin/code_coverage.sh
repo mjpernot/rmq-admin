c2c598728ec9ea8ed6f978fb868f0b2b978a284d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-24 11:25:13 +0100

    Update test/unit/rmq_admin/code_coverage.sh v0.0.1

--- test/unit/rmq_admin/code_coverage.sh
+++ test/unit/rmq_admin/code_coverage.sh
@@ -11,6 +11,7 @@ echo "Running unit test modules in conjunction with coverage"
 coverage run -a --source=rmq_admin test/unit/rmq_admin/create_base.py
 coverage run -a --source=rmq_admin test/unit/rmq_admin/fill_body.py
 coverage run -a --source=rmq_admin test/unit/rmq_admin/help_message.py 
+coverage run -a --source=rmq_admin test/unit/rmq_admin/main.py
 coverage run -a --source=rmq_admin test/unit/rmq_admin/node_health.py
 coverage run -a --source=rmq_admin test/unit/rmq_admin/print_list.py
 coverage run -a --source=rmq_admin test/unit/rmq_admin/run_program.py
